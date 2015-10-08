0b94c73e2cb991e9247e3993d5ae502db6757213
abitfan <emu@emuadmin.com>
2015-10-08 14:59:52 +0300

    PIP Packaging

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # shapeshiftio
 Python API for https://shapeshift.io
+
+Install
+=====
+
+### Using `pip`
+
+```
+$ pip install shapeshiftio
+```
+
+
+### Build from source
+
+```
+$ git clone https://github.com/abitfan/shapeshiftio
+$ cd shapeshiftio
+$ python setup.py install
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='shapeshiftio',
+        version='0.1.1',
+        description='Python API for https://shapeshift.io',
+        url='http://github.com/abitfan/shapeshiftio',
+        author='A Bit Fan',
+        author_email='abitfan@ruggedinbox.com',
+        license='MIT',
+        packages=['shapeshiftio'],
+        zip_safe=False)
